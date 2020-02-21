13763caf4a5efd52d229ecca867be92c0f277207
dell <dell@DESKTOP-IKA0ULI>
2020-02-20 21:18:14 -0500

    nuevo commit

--- webapp/index.php
+++ webapp/index.php
@@ -2,10 +2,16 @@
 	<head>
 		<meta charset="utf-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    	<meta name="author" content="Andres Bajaña">
     	<title>Biometrico</title>
     	<link href="recursos/css/bootstrap.min.css" rel="stylesheet">
 	</head>
 	<body>
-	
+		<?php 
+		include("vista/menu.php");
+		include("vista/cuerpo.php");
+		?>
+	<script type="text/javascript" src="recursos/js/jquery.js"></script>
+	<script type="text/javascript" src="recursos/js/bootstrap.min.js"></script>
 	</body>
 </html>
